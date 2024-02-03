349bb728928d8b5e4edbb529a55884046e457d12
Rubens Abacherli <rubensabacherli@gmail.com>
2024-02-03 12:05:01 -0300

    adicionando ex 004

--- 004.py
+++ 004.py
@@ -1,1 +1,9 @@
-testestestes
+"""Question: Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number. Suppose the following input is supplied to the program: 34,67,55,33,12,98 Then, the output should be: ['34', '67', '55', '33', '12', '98'] ('34', '67', '55', '33', '12', '98')
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input. tuple() method can convert list to tuple"""
+
+num = input('Escreva uma lista de números separados por ,: ')
+lis = num.split(',')
+tup = tuple(lis)
+print(lis)
+print(tup)
